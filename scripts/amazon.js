2140c37bf24496603416f7db4ba77fc03eb56d5c
Arsen Valeev <arsenvaleev123@gmail.com>
2025-02-14 17:22:16 +0500

    создана кнопка для добавления товров

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -50,7 +50,7 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add" data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
@@ -60,6 +60,30 @@ products.forEach((product) => {
 
 })
 
-console.log(productsHtml)
+
 
 document.querySelector('.js-prod').innerHTML = productsHtml
+
+document.querySelectorAll('.js-add').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId
+
+        let matItem
+        cart.forEach((item) => {
+            if (productId === item.productId){
+                matItem = item
+            }
+        })
+        if (matItem){
+            matItem.quantity += 1
+        }
+        else{
+            cart.push({
+                productId:productId,
+                quantity:1
+            })
+        }
+        console.log(cart)
+        
+    })
+})
